9f2f1b14ef15f35427a4862302f20eead38d4c8c
AlecHsu <hsu022210@gmail.com>
2015-06-19 04:41:39 +0800

    fix bug
    1. media for phone
    2. navbar pull-right
    3. hide list after click

--- js/main.js
+++ js/main.js
@@ -2,19 +2,33 @@ var scroll = function() {
 	$('body').scrollspy({ target: '#navbar' });
 };
 
+var pullright = function() {
+	$(window).resize(function() {
+		if ($(window).width() <= 768) {
+			$('#navbar').removeClass('pull-right');
+		}else{
+			$('#navbar').addClass('pull-right');
+		};	
+	});
+};
+
 var smooth = function() {
 	$('#navbar a, .slide_to_profile').click(function() {
 		$('body').animate({
 			scrollTop: $( $(this).attr('href') ).offset().top
 		}, 500);
+		
+		if ($(window).width() <= 768) {
+			if($('#navbar').css('display') !='none') {
+				$('.navbar-toggle').click();
+	        };
+		};
 
-		if($('#navbar').css('display') !='none'){
-            $(".navbar-toggle").click();
-        };
 		return false;
 	});
 };
 
-var i = [scroll, smooth]
+
+var i = [scroll, smooth, pullright]
 
 $(document).ready(i);
